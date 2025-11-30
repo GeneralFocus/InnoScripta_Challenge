657e432ad079236e26795fe25c5f09c87f963270
Generalfocus <generalfocus1@gmail.com>
2025-11-30 13:52:11 -0500

    feat: add migration for sources table

---
+++ database/migrations/2025_11_30_180019_create_sources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('provider');
+            $table->text('description')->nullable();
+            $table->string('url')->nullable();
+            $table->timestamps();
+            $table->index('provider');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sources');
+    }
+};
