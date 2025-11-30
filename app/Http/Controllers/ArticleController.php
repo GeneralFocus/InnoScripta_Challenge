e928bee1df6b60688b4d66b2784c8464667df1b3
Generalfocus <generalfocus1@gmail.com>
2025-11-30 18:21:14 -0500

    refactor: update ArticleController to support new service architecture

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,35 +6,41 @@ namespace App\Http\Controllers;
 
 use App\Services\ArticleService;
 use Illuminate\Http\JsonResponse;
-use App\Http\Resources\ArticleResource;
 use App\Http\Requests\ArticleIndexRequest;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use App\Http\Resources\ArticleResource;
 
 class ArticleController extends Controller
 {
-    public function __construct(
-        private readonly ArticleService $service
-    ) {
+    public function __construct(private readonly ArticleService $service)
+    {
     }
 
-    public function index(ArticleIndexRequest $request): AnonymousResourceCollection
+    public function index(ArticleIndexRequest $request): JsonResponse
     {
         $articles = $this->service->getArticles(
             $request->getFilters(),
             $request->getPerPage()
         );
 
-        return ArticleResource::collection($articles);
+        return $this->success(
+            'Articles retrieved',
+            ArticleResource::collection($articles)
+        );
     }
 
-    public function show(int $id): JsonResponse
+    public function show(string $id): JsonResponse
     {
-        $article = $this->service->getArticle($id);
+        if (!ctype_digit($id)) {
+            return $this->error('Invalid article id', [], 422);
+        }
+
+        $article = $this->service->getArticle((int) $id);
 
         if (!$article) {
             return $this->error('Article not found', [], 404);
         }
 
-        return $this->success('Article retrieved', new ArticleResource((object) $article));
+        return $this->success('Article retrieved', new ArticleResource($article));
+
     }
 }
