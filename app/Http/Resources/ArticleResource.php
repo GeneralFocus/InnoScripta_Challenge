20f22060b6b604f9e4248003a81ad07f9374d209
Generalfocus <generalfocus1@gmail.com>
2025-11-30 15:14:48 -0500

    refactor: update ArticleResource to standardize API response structure

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'          => $this->id,
+            'title'       => $this->title,
+            'summary'     => $this->summary,
+            'content'     => $this->content,
+            'source'      => $this->source?->name ?? $this->source,
+            'category'    => $this->category?->name ?? $this->category,
+            'author'      => $this->author,
+            'url'         => $this->url,
+            'image_url'   => $this->image_url,
+            'published_at'=> $this->published_at,
+        ];
+    }
+}
