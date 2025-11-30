d74ef4e7595710f86a9882b2e7f42af7858fa94f
Generalfocus <generalfocus1@gmail.com>
2025-11-30 18:21:14 -0500

    refactor: update API routes for new news service endpoints

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/articles')->group(function () {
     Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
-    Route::get('/{id}', [ArticleController::class, 'show'])->name('articles.show')->where('id', '[0-9]+');
+    Route::get('/{id}', [ArticleController::class, 'show'])->name('articles.show');
 });
 
 
