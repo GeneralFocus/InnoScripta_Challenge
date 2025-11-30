77a2a813bcd8d58171e03a6817332e2762b355a5
Generalfocus <generalfocus1@gmail.com>
2025-11-30 13:52:11 -0500

    feat: add migration for articles table

---
+++ database/migrations/2025_11_30_180201_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('external_id')->unique();
+            $table->foreignId('source_id');
+            $table->foreignId('category_id')->nullable();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->longText('content')->nullable();
+            $table->string('author')->nullable();
+            $table->string('url')->unique();
+            $table->string('image_url')->nullable();
+            $table->timestamp('published_at');
+            $table->timestamps();
+            $table->index('published_at');
+            $table->index('author');
+            $table->fullText(['title', 'description', 'content']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
